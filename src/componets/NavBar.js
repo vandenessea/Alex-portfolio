89cc85a6b3aafe699918f92887df1ff222263203
Alexander Vandenesse <vandenessea96@gmail.com>
2021-07-14 09:05:26 -0400

    styling-NavBar

--- src/componets/NavBar.js
+++ src/componets/NavBar.js
@@ -1,23 +1,26 @@
 import React from "react"
 import { NavLink } from "react-router-dom";
 
-
-
 export default function NavBar () {
     return (
-        <header>
-            <div>
-                <nav>
-                    <NavLink to= "/" exact>
+        <header className="bg-red-600">
+            <div className="container mx-auto flex justify-between">    
+                <nav className="flex">
+                    <NavLink to="/" exact
+                    activeClassName="text-white"
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
                         Alex
                     </NavLink>
-                    <NavLink to= "/post">
+                    <NavLink to= "/post" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700">
                         Blog Posts
                     </NavLink>
-                    <NavLink to= "/project">
+                    <NavLink to= "/project" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700">
                         Projects
                     </NavLink>
-                    <NavLink to= "/about">
+                    <NavLink to= "/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700">
                         About Me!
                     </NavLink>
                 </nav>
